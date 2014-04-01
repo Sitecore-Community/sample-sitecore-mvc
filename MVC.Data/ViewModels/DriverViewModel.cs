2777e5ca39c5d0417f318963325350019402eec2
verohari <verohari@gmail.com>
2014-04-01 15:58:36 +0100

    Updated serialization config file and commented DriverViewModel.cs

--- MVC.Data/ViewModels/DriverViewModel.cs
+++ MVC.Data/ViewModels/DriverViewModel.cs
@@ -6,6 +6,10 @@ using System.Web;
 
 namespace MVC.Data.Models
 {
+    /// <summary>
+    /// The driver view model contains data about the rendering that does not need to be stored in the actual model - like
+    /// that particular rendering's background colour, or a reference to the context item (the current page).
+    /// </summary>
     public class DriverViewModel
     {
         public Driver Driver { get; set; }
